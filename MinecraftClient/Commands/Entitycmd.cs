a9cee0d3f0a8f5ba822e3ad52fdcb4144767bf2c
1689295608 <50663849+1689295608@users.noreply.github.com>
2020-10-04 14:43:10 +0800

    汉化其他内容
    可能会出现问题

--- MinecraftClient/Commands/Entitycmd.cs
+++ MinecraftClient/Commands/Entitycmd.cs
@@ -11,7 +11,7 @@ namespace MinecraftClient.Commands
         public override string CMDName { get { return "entity"; } }
         public override string CMDDesc { get { return "entity <id|entitytype> <attack|use>"; } }
 
-        public override string Run(McClient handler, string command, Dictionary<string, object> localVars)
+        public override string Run(信息lient handler, string command, Dictionary<string, object> localVars)
         {
             if (handler.GetEntityHandlingEnabled())
             {
@@ -56,42 +56,42 @@ namespace MinecraftClient.Commands
                                             color = "§e";  // Yellow
 
                                         string location = String.Format("X:{0}, Y:{1}, Z:{2}", Math.Round(entity.Location.X, 2), Math.Round(entity.Location.Y, 2), Math.Round(entity.Location.Y, 2));
-                                        string done = String.Format("Entity: {0}\n [MCC] Type: {1}", id, type);
+                                        string done = String.Format("实体: {0}\n [信息] 类型: {1}", id, type);
                                         if (!String.IsNullOrEmpty(nickname))
-                                            done += String.Format("\n [MCC] Nickname: {0}", nickname);
+                                            done += String.Format("\n [信息] 昵称: {0}", nickname);
                                         else if (!String.IsNullOrEmpty(customname))
-                                            done += String.Format("\n [MCC] CustomName: {0}§8", customname.Replace("&", "§"));
+                                            done += String.Format("\n [信息] 自定义名称: {0}§8", customname.Replace("&", "§"));
                                         if (type == EntityType.Player)
-                                            done += String.Format("\n [MCC] Latency: {0}", latency);
+                                            done += String.Format("\n [信息] 延迟: {0}", latency);
                                         else if (type == EntityType.Item || type == EntityType.ItemFrame || type == Mapping.EntityType.EyeOfEnder || type == Mapping.EntityType.Egg || type == Mapping.EntityType.EnderPearl || type == Mapping.EntityType.Potion || type == Mapping.EntityType.Fireball || type == Mapping.EntityType.FireworkRocket)
                                         {
                                             string displayName = item.DisplayName;
                                             if (String.IsNullOrEmpty(displayName))
-                                                done += String.Format("\n [MCC] Item: {0} x{1}", item.Type, item.Count);
+                                                done += String.Format("\n [信息] 物品: {0} x{1}", item.Type, item.Count);
                                             else
-                                                done += String.Format("\n [MCC] Item: {0} x{1} - {2}§8", item.Type, item.Count, displayName);
+                                                done += String.Format("\n [信息] 物品: {0} x{1} - {2}§8", item.Type, item.Count, displayName);
                                         }
 
                                         if (entity.Equipment.Count >= 1 && entity.Equipment != null)
                                         {
-                                            done += String.Format("\n [MCC] Equipment:");
+                                            done += String.Format("\n [信息] 设备:");
                                             if (entity.Equipment.ContainsKey(0) && entity.Equipment[0] != null)
-                                                done += String.Format("\n   [MCC] MainHand: {0} x{1}", entity.Equipment[0].Type, entity.Equipment[0].Count);
+                                                done += String.Format("\n   [信息] 主手: {0} x{1}", entity.Equipment[0].Type, entity.Equipment[0].Count);
                                             if (entity.Equipment.ContainsKey(1) && entity.Equipment[1] != null)
-                                                done += String.Format("\n   [MCC] OffHand: {0} x{1}", entity.Equipment[1].Type, entity.Equipment[1].Count);
+                                                done += String.Format("\n   [信息] 副手: {0} x{1}", entity.Equipment[1].Type, entity.Equipment[1].Count);
                                             if (entity.Equipment.ContainsKey(5) && entity.Equipment[5] != null)
-                                                done += String.Format("\n   [MCC] Helmet: {0} x{1}", entity.Equipment[5].Type, entity.Equipment[5].Count);
+                                                done += String.Format("\n   [信息] 头盔: {0} x{1}", entity.Equipment[5].Type, entity.Equipment[5].Count);
                                             if (entity.Equipment.ContainsKey(4) && entity.Equipment[4] != null)
-                                                done += String.Format("\n   [MCC] Chestplate: {0} x{1}", entity.Equipment[4].Type, entity.Equipment[4].Count);
+                                                done += String.Format("\n   [信息] 胸甲: {0} x{1}", entity.Equipment[4].Type, entity.Equipment[4].Count);
                                             if (entity.Equipment.ContainsKey(3) && entity.Equipment[3] != null)
-                                                done += String.Format("\n   [MCC] Leggings: {0} x{1}", entity.Equipment[3].Type, entity.Equipment[3].Count);
+                                                done += String.Format("\n   [信息] 护腿: {0} x{1}", entity.Equipment[3].Type, entity.Equipment[3].Count);
                                             if (entity.Equipment.ContainsKey(2) && entity.Equipment[2] != null)
-                                                done += String.Format("\n   [MCC] Boots: {0} x{1}", entity.Equipment[2].Type, entity.Equipment[2].Count);
+                                                done += String.Format("\n   [信息] 鞋子: {0} x{1}", entity.Equipment[2].Type, entity.Equipment[2].Count);
                                         }
-                                        done += String.Format("\n [MCC] Pose: {0}", pose);
-                                        done += String.Format("\n [MCC] Health: {0}", color + health + "§8");
-                                        done += String.Format("\n [MCC] Distance: {0}", distance);
-                                        done += String.Format("\n [MCC] Location: {0}", location);
+                                        done += String.Format("\n [信息] 构成: {0}", pose);
+                                        done += String.Format("\n [信息] 血量: {0}", color + health + "§8");
+                                        done += String.Format("\n [信息] 距离: {0}", distance);
+                                        done += String.Format("\n [信息] 位置: {0}", location);
                                         return done;
                                 }
                             }
@@ -101,7 +101,7 @@ namespace MinecraftClient.Commands
                         {
                             EntityType interacttype = EntityType.Player;
                             Enum.TryParse(args[0], out interacttype);
-                            string actionst = "Entity attacked";
+                            string actionst = "已攻击该实体";
                             int actioncount = 0;
                             foreach (var entity2 in handler.GetEntities())
                             {
@@ -113,13 +113,13 @@ namespace MinecraftClient.Commands
                                     if (action == "attack")
                                     {
                                         handler.InteractEntity(entity2.Key, 1);
-                                        actionst = "Entity attacked";
+                                        actionst = "已攻击该实体";
                                         actioncount++;
                                     }
                                     else if (action == "use")
                                     {
                                         handler.InteractEntity(entity2.Key, 0);
-                                        actionst = "Entity used";
+                                        actionst = "已使用该实体";
                                         actioncount++;
                                     }
                                     else return CMDDesc;
@@ -134,7 +134,7 @@ namespace MinecraftClient.Commands
                 {
                     Dictionary<int, Mapping.Entity> entities = handler.GetEntities();
                     List<string> response = new List<string>();
-                    response.Add("Entities:");
+                    response.Add("实体:");
                     foreach (var entity2 in entities)
                     {
                         int id = entity2.Key;
@@ -148,19 +148,19 @@ namespace MinecraftClient.Commands
                         string location = String.Format("X:{0}, Y:{1}, Z:{2}", Math.Round(entity2.Value.Location.X, 2), Math.Round(entity2.Value.Location.Y, 2), Math.Round(entity2.Value.Location.Y, 2));
 
                         if (type == EntityType.Item || type == EntityType.ItemFrame || type == Mapping.EntityType.EyeOfEnder || type == Mapping.EntityType.Egg || type == Mapping.EntityType.EnderPearl || type == Mapping.EntityType.Potion || type == Mapping.EntityType.Fireball || type == Mapping.EntityType.FireworkRocket)
-                            response.Add(String.Format(" #{0}: Type: {1}, Item: {2}, Location: {3}", id, type, item.Type, location));
+                            response.Add(String.Format(" #{0}: 类型: {1}, 物品: {2}, 位置: {3}", id, type, item.Type, location));
                         else if (type == Mapping.EntityType.Player && !String.IsNullOrEmpty(nickname))
-                            response.Add(String.Format(" #{0}: Type: {1}, Nickname: §8{2}§8, Latency: {3}, Health: {4}, Pose: {5}, Location: {6}", id, type, nickname, latency, health, pose, location));
+                            response.Add(String.Format(" #{0}: 类型: {1}, 昵称: §8{2}§8, 延迟: {3}, 血量: {4}, 构成: {5}, 位置: {6}", id, type, nickname, latency, health, pose, location));
                         else if (type == Mapping.EntityType.Player && !String.IsNullOrEmpty(customname))
-                            response.Add(String.Format(" #{0}: Type: {1}, CustomName: §8{2}§8, Latency: {3}, Health: {4}, Pose: {5}, Location: {6}", id, type, customname.Replace("&", "§"), latency, health, pose, location));
+                            response.Add(String.Format(" #{0}: 类型: {1}, 自定义名称: §8{2}§8, 延迟: {3}, 血量: {4}, 构成: {5}, 位置: {6}", id, type, customname.Replace("&", "§"), latency, health, pose, location));
                         else
-                            response.Add(String.Format(" #{0}: Type: {1}, Health: {2}, Location: {3}", id, type, health, location));
+                            response.Add(String.Format(" #{0}: 类型: {1}, 血量: {2}, 位置: {3}", id, type, health, location));
                     }
                     response.Add(CMDDesc);
                     return String.Join("\n", response);
                 }
             }
-            else return "请先开启 entityhandling.";
+            else return "请先开启 Entityhandling.";
         }
     }
 }
