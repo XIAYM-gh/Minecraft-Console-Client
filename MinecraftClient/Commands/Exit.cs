6f6e3bacd397fd8267f4520f38de84c67c34281d
1689295608 <50663849+1689295608@users.noreply.github.com>
2020-10-05 12:03:21 +0800

    Exit貌似不会关闭程序的叭？

--- MinecraftClient/Commands/Exit.cs
+++ MinecraftClient/Commands/Exit.cs
@@ -8,13 +8,10 @@ namespace MinecraftClient.Commands
     public class Exit : Command
     {
         public override string CMDName { get { return "exit"; } }
-        public override string CMDDesc { get { return "exit: 从服务器断开."; } }
+        public override string CMDDesc { get { return "exit: 断开与服务器的连接"; } }
 
         public override string Run(McClient handler, string command, Dictionary<string, object> localVars)
         {
-            MCC.LogToConsole("正在关闭 Minecraft Console Client 汉化版...");
-            Thread.Sleep(1000);
-            MCC.LogToConsole("再会哦~ :)");
             Program.Exit();
             return "";
         }
