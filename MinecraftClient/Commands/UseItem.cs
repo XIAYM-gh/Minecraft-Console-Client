7f3bb0be8cc251306737c366c8988c7b91f14330
1689295608 <50663849+1689295608@users.noreply.github.com>
2020-10-05 18:58:33 +0800

    Update UseItem.cs

--- MinecraftClient/Commands/UseItem.cs
+++ MinecraftClient/Commands/UseItem.cs
@@ -8,14 +8,14 @@ namespace MinecraftClient.Commands
     class UseItem : Command
     {
         public override string CMDName { get { return "useitem"; } }
-        public override string CMDDesc { get { return "useitem: 左键手上的物品."; } }
+        public override string CMDDesc { get { return "useitem: 左键手上的物品"; } }
 
         public override string Run(McClient handler, string command, Dictionary<string, object> localVars)
         {
             if (handler.GetInventoryEnabled())
             {
                 handler.UseItemOnHand();
-                return "已左键物品.";
+                return "已左键物品";
             }
             else return "请先启用 inventoryhandling.";
         }
