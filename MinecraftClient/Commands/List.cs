1d2435b33e742bf63463a4b18a714544dc300145
1689295608 <50663849+1689295608@users.noreply.github.com>
2020-10-05 12:04:32 +0800

    修正不准确的翻译

--- MinecraftClient/Commands/List.cs
+++ MinecraftClient/Commands/List.cs
@@ -8,7 +8,7 @@ namespace MinecraftClient.Commands
     public class List : Command
     {
         public override string CMDName { get { return "list"; } }
-        public override string CMDDesc { get { return "list: 查询玩家列表."; } }
+        public override string CMDDesc { get { return "list: 显示服务器在线的玩家"; } }
 
         public override string Run(McClient handler, string command, Dictionary<string, object> localVars)
         {
