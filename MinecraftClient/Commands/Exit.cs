5ba6abfece5b9b3f89f866f25bfd57e069e47b71
XIAYM <66472712+XIAYM-gh@users.noreply.github.com>
2020-10-05 11:58:23 +0800

    更新描述

--- MinecraftClient/Commands/Exit.cs
+++ MinecraftClient/Commands/Exit.cs
@@ -8,11 +8,13 @@ namespace MinecraftClient.Commands
     public class Exit : Command
     {
         public override string CMDName { get { return "exit"; } }
-        public override string CMDDesc { get { return "exit: disconnect from the server."; } }
+        public override string CMDDesc { get { return "exit: 从服务器断开."; } }
 
         public override string Run(McClient handler, string command, Dictionary<string, object> localVars)
         {
-            MCC.LogToConsole("正在关闭 Minecraft Console Client ChineseTranslate...");
+            MCC.LogToConsole("正在关闭 MCC汉化版");
+            MCC.LogToConsole("感谢您的使用:)");
+            MCC.LogToConsole("再会!");
             Thread.Sleep(1000);
             Program.Exit();
             return "";
