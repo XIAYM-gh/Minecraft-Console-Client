35e4158ea8b81f21285e09b344aeb61d5ae21910
1689295608 <50663849+1689295608@users.noreply.github.com>
2020-10-05 12:44:18 +0800

    劳资的名字呢

--- MinecraftClient/Program.cs
+++ MinecraftClient/Program.cs
@@ -44,7 +44,8 @@ namespace MinecraftClient
         static void Main(string[] args)
         {
             Console.WriteLine("Console Client for MC {0} to {1} - v{2} - By ORelio & Contributors", MCLowestVersion, MCHighestVersion, Version);
-			ConsoleIO.WriteLineFormatted("§e更新到 (ORelio) Commit ID 0c88c18ea060853b32d5b23684d9323bfd3840ae\n§8汉化由 XIAYM 与 WindowX 共同完成");//劳资的名字捏！
+	    ConsoleIO.WriteLineFormatted("§7已更新到 (ORelio) Commit ID 0c88c18ea060853b32d5b23684d9323bfd3840ae\n§e本程序的汉化由 XIAYM 与 WindowX 共同完成~");
+	    //劳资的名字捏！！！！
             //Build information to facilitate processing of bug reports
             //if (BuildInfo != null)
             //{
