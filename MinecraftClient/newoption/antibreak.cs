1f923c7ef0843970ac01cc768768d8e1d7acea29
XIAYM <66472712+XIAYM-gh@users.noreply.github.com>
2020-10-07 16:30:45 +0800

    Update antibreak.cs

--- MinecraftClient/newoption/antibreak.cs
+++ MinecraftClient/newoption/antibreak.cs
@@ -2,7 +2,9 @@ public class MyTextBox:TextBox
 {
 public const int WM_COPY=0x301;
 public const int WM_CUT=0x300;
-protected overide void WndProc(ref Message m)
+//Retry!
+WndProc(ref Message m)
+protected override void WndProc(ref Message m)
 {
 if (e.Msg==WM_COPY||e.Msg ==WM_CUT)return;//不处理
 base.WndProc(ref m);
