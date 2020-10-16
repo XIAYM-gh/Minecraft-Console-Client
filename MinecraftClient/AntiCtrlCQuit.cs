ee7ba170ccbee567f37fc9e6960032bba828eb06
XIAYM <66472712+XIAYM-gh@users.noreply.github.com>
2020-10-16 20:44:47 +0800

    Update AntiCtrlCQuit.cs

--- MinecraftClient/AntiCtrlCQuit.cs
+++ MinecraftClient/AntiCtrlCQuit.cs
@@ -1,16 +1,26 @@
 using System;
-using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using MinecraftClient.Protocol.Session;
+using MinecraftClient.Protocol;
+namespace MinecraftClient
+{
 //反Ctrl+C退出
 // MinecraftClient/AntiCtrlCQuit.cs
 public class Example
 {
     public static void Main()
+
     {
         AppDomain appd = AppDomain.CurrentDomain;
         Console.CancelKeyPress += (sender, e) =>
         {
             e.Cancel = true; //true: 不导致退出。false: 会导致退出
-            Console.WriteLine("You have Press Ctrl+C");
+            ConsoleIO.WriteLineFormatted("你正在尝试按下Ctrl+C关闭程序，但被取消了操作.");
         };
     }
+}
 }
